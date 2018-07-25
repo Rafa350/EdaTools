c640acae68450481ed2fe34766ea0cadd015f0d8
Rafael Serrano <rsr.private@gmail.com>
2018-07-26 00:10:29 +0200

    Commit final del dia
    Signed-off-by: Rafael Serrano <rsr.private@gmail.com>

--- Data/datagen.bat
+++ Data/datagen.bat
@@ -1,13 +1,11 @@
-@echo off
+rem @echo off
 
 rem Genera els fitxers de produccio PCB
 "..\bin\x86\Release\EdaCAMTool.exe" board3.xml
-"c:\program files\winrar\rar.exe" a data.zip *.gbr *.ipc 
-move /y data.zip Production\eCircuits\board3_CAM.zip
 
 rem Genera els fitxers de fabricacio P&P
 "..\bin\x86\Release\EdaExtractor.exe" board3.xml
-"..\..\MdXMLToolsV1\bin\x86\Release\MdXMLTool.exe" board3_PartList.xml ..\XSL\Export_eCircuits_BOM.xsl Production\eCircuits\board3_BOM.csv
-"..\..\MdXMLToolsV1\bin\x86\Release\MdXMLTool.exe" board3_PartList.xml ..\XSL\Export_eCircuits_CPL.xsl Production\eCircuits\board3_CPL.csv
+"..\..\XMLTools\bin\x86\Release\MdXMLTool.exe" board3_PartList.xml ..\XSL\Export_eCircuits_BOM.xsl Production\eCircuits\board3_BOM.csv
+"..\..\XMLTools\bin\x86\Release\MdXMLTool.exe" board3_PartList.xml ..\XSL\Export_eCircuits_CPL.xsl Production\eCircuits\board3_CPL.csv
 
 pause
