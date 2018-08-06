30b45fe7ba3c152dd4882bdd157902adf8562408
Rafael Serrano <rsr.private@gmail.com>
2018-08-06 17:47:12 +0200

    Gnenerador CAM per fitxers DXS del perfil de la placa
    Signed-off-by: Rafael Serrano <rsr.private@gmail.com>

---
+++ EdaCAMLibrary/DXF/Builder/DXFBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace MikroPic.EdaTools.v1.Cam.DXF.Builder {
+
+    using System;
+    using System.IO;
+
+    public sealed class DXFBuilder {
+
+        private readonly TextWriter writer;
+
+        public DXFBuilder(TextWriter writer) {
+
+            if (writer == null)
+                throw new ArgumentNullException("writer");
+
+            this.writer = writer;
+        }
+    }
+}
---
+++ EdaCAMLibrary/DXF/DXFGenerator.cs
@@ -0,0 +1,40 @@
+﻿namespace MikroPic.EdaTools.v1.Cam.DXF {
+
+    using System;
+    using System.IO;
+    using MikroPic.EdaTools.v1.Pcb.Model;
+    using MikroPic.EdaTools.v1.Cam.DXF.Builder;
+
+    public sealed class DXFGenerator {
+
+        private readonly Board board;
+
+        public DXFGenerator(Board board) {
+
+            if (board == null)
+                throw new ArgumentNullException("board");
+
+            this.board = board;
+        }
+
+        public void GenerateContent(TextWriter writer) {
+
+            if (writer == null)
+                throw new ArgumentNullException("writer");
+
+
+            DXFBuilder db = new DXFBuilder(writer);
+
+            GenerateHeader(db);
+            GenerateProfile(db);
+        }
+
+        private void GenerateHeader(DXFBuilder db) {
+
+        }
+
+        private void GenerateProfile(DXFBuilder db) {
+
+        }
+    }
+}
