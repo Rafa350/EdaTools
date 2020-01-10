8a33ee3b46541df04242c6c846a49db899c9a405
Rafael Serrano <rsr.openware@gmail.com>
2020-01-10 17:36:04 +0100

    Millores en matrius 2D. Obtingut de AvaloniaUI
    Signed-off-by: Rafael Serrano <rsr.openware@gmail.com>

--- v2/EdaTools/EdaBase/Geometry/Utils/Matrix2D.cs
+++ v2/EdaTools/EdaBase/Geometry/Utils/Matrix2D.cs
@@ -1,154 +1,228 @@
 ﻿namespace MikroPic.EdaTools.v1.Base.Geometry.Utils {
 
+    // Basat el Avalonia.Matrix
+
     using System;
 
-    public struct Matrix2D {
+    public readonly struct Matrix2D : IEquatable<Matrix2D> {
 
-        [Flags]
-        private enum MatrixType {
-            Identity = 0,
-            Translation = 1,
-            Scale = 2,
-            Unknown = 4
-        }
-
-        private double[,] m;
-        private MatrixType type;
+        private readonly double m11;
+        private readonly double m12;
+        private readonly double m21;
+        private readonly double m22;
+        private readonly double tx;
+        private readonly double ty;
 
         /// <summary>
-        /// Afegeix una operacio de translacio.
+        /// Constructor.
         /// </summary>
-        /// <param name="tx">Coordinada X de la traslacio.</param>
-        /// <param name="ty">Coordinada Y de la traslacio.</param>
+        /// <param name="m11">Parametre m11</param>
+        /// <param name="m12">Parametre m12</param>
+        /// <param name="m21">Parametre m21</param>
+        /// <param name="m22">Parametre m22</param>
+        /// <param name="tx">Parametre tx</param>
+        /// <param name="ty">Parametre ty</param>
         /// 
-        public void Translate(double tx, double ty) {
+        public Matrix2D(double m11, double m12, double m21, double m22, double tx, double ty) {
 
-            switch (type) {
-
-                case MatrixType.Identity: {
-                    m[2, 0] = tx;
-                    m[2, 1] = ty;
-                    type = MatrixType.Translation;
-                    break;
-                }
-
-                case MatrixType.Unknown: {
-                    m[2, 0] += tx;
-                    m[2, 1] += ty;
-                    break;
-                }
-
-                default: {
-                    m[2, 0] += tx;
-                    m[2, 1] += ty;
-                    type |= MatrixType.Translation;
-                    break;
-                }
-            }
+            this.m11 = m11;
+            this.m12 = m12;
+            this.m21 = m21;
+            this.m22 = m22;
+            this.tx = tx;
+            this.ty = ty;
         }
 
         /// <summary>
-        /// Afegeix una operacio d'escalat.
+        /// Crea una matriu identitat.
         /// </summary>
-        /// <param name="sx">Factor d'escala X.</param>
-        /// <param name="sy">Factor d'escala Y.</param>
+        /// <returns>La matriu.</returns>
         /// 
-        public void Scale(double sx, double sy) {
+        public static Matrix2D CreateIdentity() {
 
-            ScaleAt(sx, sy, 0, 0);
+            return new Matrix2D(1, 0, 0, 1, 0, 0);
         }
 
         /// <summary>
-        /// Afegeix una operacio d'escalat.
+        /// Crea una matriu de translacio.
+        /// </summary>
+        /// <param name="tx">Translacio en l'eix X.</param>
+        /// <param name="ty">Translacio en l'eix Y.</param>
+        /// <returns>La matriu.</returns>
+        /// 
+        public static Matrix2D CreateTranslation(double tx, double ty) {
+
+            return new Matrix2D(1, 0, 0, 1, tx, ty);
+        }
+
+        /// <summary>
+        /// Crea una matriu de escalat.
         /// </summary>
         /// <param name="sx">Factor d'escala X.</param>
         /// <param name="sy">Factor d'escala Y.</param>
-        /// <param name="ox">Coordinada X de l'origen.</param>
-        /// <param name="oy">Coordinada Y de l'origen.</param>
+        /// <returns>La matriu.</returns>
         /// 
-        public void ScaleAt(double sx, double sy, double ox, double oy) {
+        public static Matrix2D CreateScale(double sx, double sy) {
 
-            double[,] sm = new double[3, 3];
-
-            sm[0, 0] = sx;
-            sm[0, 1] = 0;
-            sm[0, 2] = 0;
-
-            sm[1, 0] = 0;
-            sm[1, 1] = sy;
-            sm[1, 2] = 0;
-
-            if (ox == 0) // Optimitza el cas ox == 0
-                sm[2, 0] = 0;
-            else
-                sm[2, 0] = (1 - sx) * ox;
-            if (oy == 0) // Optimitza el cas oy == 0
-                sm[2, 1] = 0;
-            else
-                sm[2, 1] = (1 - sy) * oy;
-            sm[2, 2] = 1;
-
-            CombineMatrix(sm, MatrixType.Scale);
+            return new Matrix2D(sx, 0, 0, sy, 0, 0);
         }
 
         /// <summary>
-        /// Afegeix una operacio de rotacio.
+        /// Crea una matriu d'escalat.
         /// </summary>
-        /// <param name="a">Angle de rotacio.</param>
+        /// <param name="sx">Factor d'escala X.</param>
+        /// <param name="sy">Factor d'escala Y.</param>
+        /// <param name="ox">Origen X del escalat.</param>
+        /// <param name="oy">Origen Y del escalat.</param>
+        /// <returns>La matriu.</returns>
         /// 
-        public void Rotate(double a) {
+        public static Matrix2D CreateScale(double sx, double sy, double ox, double oy) {
 
-            RotateAt(a, 0, 0);
+            return new Matrix2D(sx, 0, 0, sy, ox - sx * ox, oy - sy * oy);
         }
 
-        /// <summary>
-        /// Afegeix una operacio de rotacio.
-        /// </summary>
-        /// <param name="a">Angle de rotacio.</param>
-        /// <param name="ox">Coordinada X de l'origen.</param>
-        /// <param name="oy">Coordinada Y de l'origen.</param>
-        /// 
-        public void RotateAt(double a, double ox, double oy) {
-
-            double[,] rm = new double[3, 3];
+        public static Matrix2D CreateRotation(double a) {
 
             double rad = (a % 360.0) * Math.PI / 180.0;
             double sin = Math.Sin(rad);
             double cos = Math.Cos(rad);
 
-            rm[0, 0] = cos;
-            rm[0, 1] = sin;
-            rm[0, 2] = 0;
-
-            rm[1, 0] = -sin;
-            rm[1, 1] = cos;
-            rm[1, 2] = 0;
-
-            if ((ox == 0) && (oy == 0)) {
-                rm[2, 0] = 0;
-                rm[2, 1] = 0;
-            }
-            else {
-                rm[2, 0] = ox * (1 - cos) + oy * sin;
-                rm[2, 1] = oy * (1 - cos) - ox * sin;
-            }
-            rm[2, 2] = 1;
-
-            CombineMatrix(rm, MatrixType.Unknown);
+            return new Matrix2D(cos, sin, -sin, cos, 0, 0);
         }
 
+        public static Matrix2D CreateRotation(double a, double ox, double oy) {
 
-        public void Multiply(Matrix2D matrix) {
+            double rad = (a % 360.0) * Math.PI / 180.0;
+            double sin = Math.Sin(rad);
+            double cos = Math.Cos(rad);
+            double tx = (ox * (1.0 - cos)) + (oy * sin);
+            double ty = (oy * (1.0 - cos)) - (ox * sin);
 
-            if (matrix.type == MatrixType.Identity)
-                return;
+            return new Matrix2D(cos, sin, -sin, cos, tx, ty);
+        }
 
-            if (type == MatrixType.Identity) {
-                m = matrix.m;
-                return;
-            }
+        /// <summary>
+        /// Calculates the determinant for this matrix.
+        /// </summary>
+        /// <returns>The determinant.</returns>
+        /// <remarks>
+        /// The determinant is calculated by expanding the matrix with a third column whose
+        /// values are (0,0,1).
+        /// </remarks>
+        /// 
+        public double GetDeterminant() {
+        
+            return (m11 * m22) - (m12 * m21);
+        }
 
-            CombineMatrix(matrix.m, matrix.type);
+        /// <summary>
+        /// Inverts the Matrix.
+        /// </summary>
+        /// <returns>The inverted matrix.</returns>
+        /// 
+        public Matrix2D Invert() {
+
+            double d = GetDeterminant();
+            if (d == 0) 
+                throw new InvalidOperationException("Transform is not invertible.");
+
+            return new Matrix2D(
+                m22 / d,
+                -m12 / d,
+                -m21 / d,
+                m11 / d,
+                ((m21 * ty) - (m22 * tx)) / d,
+                ((m12 * tx) - (m11 * ty)) / d);
+        }
+
+        /// <summary>
+        /// Multiplies two matrices together and returns the resulting matrix.
+        /// </summary>
+        /// <param name="value1">The first source matrix.</param>
+        /// <param name="value2">The second source matrix.</param>
+        /// <returns>The product matrix.</returns>
+        /// 
+        public static Matrix2D operator *(Matrix2D value1, Matrix2D value2) {
+            
+            return new Matrix2D(
+                (value1.m11 * value2.m11) + (value1.m12 * value2.m21),
+                (value1.m11 * value2.m12) + (value1.m12 * value2.m22),
+                (value1.m21 * value2.m11) + (value1.m22 * value2.m21),
+                (value1.m21 * value2.m12) + (value1.m22 * value2.m22),
+                (value1.tx * value2.m11) + (value1.ty * value2.m21) + value2.tx,
+                (value1.tx * value2.m12) + (value1.ty * value2.m22) + value2.ty);
+        }
+
+        /// <summary>
+        /// Negates the given matrix by multiplying all values by -1.
+        /// </summary>
+        /// <param name="value">The source matrix.</param>
+        /// <returns>The negated matrix.</returns>
+        /// 
+        public static Matrix2D operator -(Matrix2D value) {
+
+            return value.Invert();
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the given matrices are equal.
+        /// </summary>
+        /// <param name="value1">The first source matrix.</param>
+        /// <param name="value2">The second source matrix.</param>
+        /// <returns>True if the matrices are equal; False otherwise.</returns>
+        /// 
+        public static bool operator ==(Matrix2D value1, Matrix2D value2) {
+            
+            return value1.Equals(value2);
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the given matrices are not equal.
+        /// </summary>
+        /// <param name="value1">The first source matrix.</param>
+        /// <param name="value2">The second source matrix.</param>
+        /// <returns>True if the matrices are not equal; False if they are equal.</returns>
+        /// 
+        public static bool operator !=(Matrix2D value1, Matrix2D value2) {
+            
+            return !value1.Equals(value2);
+        }
+
+        public bool Equals(Matrix2D other) {
+
+            return
+                m11 == other.m11 &&
+                m12 == other.m12 &&
+                m21 == other.m21 &&
+                m22 == other.m22 &&
+                tx == other.tx &&
+                ty == other.ty;
+        }
+
+        /// <summary>
+        /// Returns a boolean indicating whether the given Object is equal to this matrix instance.
+        /// </summary>
+        /// <param name="obj">The Object to compare against.</param>
+        /// <returns>True if the Object is equal to this matrix; False otherwise.</returns>
+        public override bool Equals(object obj) {
+
+            return obj is Matrix2D other && Equals(other);
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>The hash code.</returns>
+        /// 
+        public override int GetHashCode() {
+
+            return 
+                m11.GetHashCode() + 
+                m12.GetHashCode() +
+                m21.GetHashCode() + 
+                m22.GetHashCode() +
+                tx.GetHashCode() + 
+                ty.GetHashCode();
         }
 
         /// <summary>
@@ -159,156 +233,9 @@
         /// 
         public void Apply(ref double x, ref double y) {
 
-            switch (type) {
-                
-                case MatrixType.Identity:
-                    break;
-
-                case MatrixType.Translation:
-                    x += m[2, 0];
-                    y += m[2, 1];
-                    break;
-
-                case MatrixType.Scale:
-                    x *= m[0, 0];
-                    y *= m[1, 1];
-                    break;
-
-                case MatrixType.Translation | MatrixType.Scale:
-                    x *= m[0, 0];
-                    x += m[2, 0];
-                    y *= m[1, 1];
-                    y += m[2, 1];
-                    break;
-
-                default: {
-                    double xx = x;
-                    x = xx * m[0, 0] + y * m[1, 0] + m[2, 0];
-                    y = xx * m[0, 1] + y * m[1, 1] + m[2, 1];
-                    break;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Combina amb un altre matriu.
-        /// </summary>
-        /// <param name="cm">La matriu per combinar.</param>
-        /// <param name="ct">Tipus de matriu a combinar.</param>
-        /// 
-        private void CombineMatrix(double[,] cm, MatrixType ct) {
-
-            double[,] rm = new double[3, 3];
-
-            for (int r = 0; r < 3; r++)
-                for (int c = 0; c < 3; c++) {
-                    double sum = 0;
-                    for (int z = 0; z < 3; z++)
-                        sum += m[r, z] * cm[z, c];
-                    rm[r, c] = sum;
-                }
-
-            m = rm;
-
-            switch (type) {
-                case MatrixType.Identity:
-                    type = ct;
-                    break;
-
-                case MatrixType.Translation:
-                case MatrixType.Scale:
-                case MatrixType.Translation | MatrixType.Scale:
-                    if (ct == MatrixType.Unknown)
-                        type = MatrixType.Unknown;
-                    else
-                        type |= ct;
-                    break;
-
-                case MatrixType.Unknown:
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Asigna els parametres de la matriu.
-        /// </summary>
-        /// <param name="m11">Parametre m11.</param>
-        /// <param name="m12">Parametre m12.</param>
-        /// <param name="m21">Parametre m21.</param>
-        /// <param name="m22">Parametre m22.</param>
-        /// <param name="tx">Parametre de translacio X.</param>
-        /// <param name="ty">Parametre de traslacio Y.</param>
-        /// <param name="type">Tipus de matriu.</param>
-        /// 
-        private void SetMatrix(double m11, double m12, double m21, double m22, double tx, double ty, MatrixType type) {
-
-            m = new double[3, 3];
-
-            m[0, 0] = m11;
-            m[0, 1] = m12;
-            m[0, 2] = 0;
-
-            m[1, 0] = m21;
-            m[1, 1] = m22;
-            m[1, 2] = 0;
-
-            m[2, 0] = tx;
-            m[2, 1] = ty;
-            m[2, 2] = 1;
-
-            this.type = type;
-        }
-
-        /// <summary>
-        /// Crea una matriu identitat.
-        /// </summary>
-        /// <returns>La matriu.</returns>
-        /// 
-        private static Matrix2D CreateIdentity() {
-
-            Matrix2D m = new Matrix2D();
-            m.SetMatrix(1, 0, 0, 1, 0, 0, MatrixType.Identity);
-
-            return m;
-        }
-
-        /// <summary>
-        /// Crea una matriu d'escalat.
-        /// </summary>
-        /// <param name="sx">Factor d'escala X.</param>
-        /// <param name="sy">Factor d'escala Y.</param>
-        /// <param name="ox">Coordinada X de l'origen.</param>
-        /// <param name="oy">Coordinada Y de l'origen.</param>
-        /// <returns>La matriu.</returns>
-        /// 
-        private static Matrix2D CreateScale(double sx, double sy, double ox, double oy) {
-
-            Matrix2D m = new Matrix2D();
-            m.SetMatrix(sx, 0, 0, sy, ox - sx * ox, oy - sy * oy, MatrixType.Scale | MatrixType.Translation);
-
-            return m;
-        }
-
-        /// <summary>
-        /// Crea una matriu de rotacio.
-        /// </summary>
-        /// <param name="a">Angle de rotacio.</param>
-        /// <param name="ox">Coordinada X del centre de rotacio.</param>
-        /// <param name="oy">Coordinada Y del centre de rotacio.</param>
-        /// <returns>La matriu.</returns>
-        /// 
-        private static Matrix2D CreateRotate(double a, double ox, double oy) {
-
-            double rad = (a % 360.0) * Math.PI / 180.0;
-            double sin = Math.Sin(rad);
-            double cos = Math.Cos(rad);
-            double tx = (ox * (1.0 - cos)) + (oy * sin);
-            double ty = (oy * (1.0 - cos)) - (ox * sin);
-
-            Matrix2D m = new Matrix2D();
-            m.SetMatrix(cos, sin, -sin, cos, tx, ty, MatrixType.Unknown);
-            
-            return m;
+            double xx = x;
+            x = xx * m11 + y * m21 + tx;
+            y = xx * m12 + y * m22 + ty;
         }
 
         /// <summary>
@@ -323,40 +250,38 @@
 
         public double M11 {
             get {
-                return m[0, 0];
+                return m11;
             }
         }
 
         public double M12 {
             get {
-                return m[0, 1];
+                return m12;
             }
         }
 
         public double M21 {
             get {
-                return m[1, 0];
+                return m21;
             }
         }
 
         public double M22 {
             get {
-                return m[1, 1];
+                return m22;
             }
         }
 
         public double OffsetX {
             get {
-                return m[2, 0];
+                return tx;
             }
         }
 
         public double OffsetY {
             get {
-                return m[2, 1];
+                return ty;
             }
         }
     }
-
 }
-
