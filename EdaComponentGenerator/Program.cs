f8604b899d25d837460d17b0734745e5825ae9a7
Rafael Serrano <rsr.openware@gmail.com>
2021-05-12 15:37:51 +0200

    Nova aplicacio EdaCOmpònentGenerator
    Signed-off-by: Rafael Serrano <rsr.openware@gmail.com>

---
+++ EdaComponentGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EdaComponentGenerator {
+    
+    class Program {
+        
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
