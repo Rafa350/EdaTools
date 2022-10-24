392ae148eee6cbdcb59fe17574ff7a77c40172dc
Rafael Serrano <rsr.openware@gmail.com>
2022-10-24 21:49:34 +0200

    CGenerador de LogicalNet
    Signed-off-by: Rafael Serrano <rsr.openware@gmail.com>

---
+++ EdaCoreExtensions/LogicalNet/EdaLogicalNet.cs
@@ -0,0 +1,15 @@
+﻿namespace MikroPic.EdaTools.v1.CoreExtensions.LogicalNet {
+    
+    public sealed class EdaLogicalNet {
+
+        private readonly List<EdaLogicalNetEntry> _nets;
+
+        public EdaLogicalNet(IList<EdaLogicalNetEntry> nets) {
+
+            _nets = new List<EdaLogicalNetEntry>(nets);
+        }
+
+        public IEnumerable<EdaLogicalNetEntry> Nets =>
+            _nets;
+    }
+}
